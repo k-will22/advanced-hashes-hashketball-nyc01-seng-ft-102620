4ddbf451e5237262e9c36941dff857ed10108c3d
Keith Williams <thestreetkw@hotmail.com>
2020-10-17 08:17:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -236,11 +236,11 @@ def most_points_scored
 end 
 
 def winning_team
-  home_players_points = game_hash[:home][:players][:points].to_i
-  away_players_points = game_hash[:away][:players][:points].to_i
+  home_players_points = game_hash[:home][:players][:points].to_i.sum 
+  away_players_points = game_hash[:away][:players][:points].to_i.sum
   if home_players_points.sum > away_players_points.sum 
     return game_hash[:home]
-      elsif home_players_points.sum == away_players_points.sum 
+      elsif home_players_points == away_players_points 
     return "tie"
       else 
     return game_hash[:away]
