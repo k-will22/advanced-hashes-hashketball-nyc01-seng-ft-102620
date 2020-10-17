78f7dff57b83a3e92b18110dc3a0ef0daf29e2b2
Keith Williams <thestreetkw@hotmail.com>
2020-10-17 04:28:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,7 +128,7 @@ end
 
 # Write code here
 def num_points_scored(player)
-  game_hash().each do |location, data| 
+  game_hash.each do |location, data| 
     data[:players].each do |name, stat| 
       if player == name 
         return stat[:points]
