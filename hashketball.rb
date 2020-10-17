c5841a235937dec794211fb0bedd32cdf9c3db24
Keith Williams <thestreetkw@hotmail.com>
2020-10-17 07:41:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -200,7 +200,7 @@ def find_player_with_longest_shoe
   largest_shoe_size = nil 
   player_with_largest_shoe = nil 
   game_hash.each do |city, data|
-    data[:players].each do |name, stats|
+    data[:players].each.key do |name, stats|
 binding.pry 
       if largest_shoe_size = nil || stats[:shoe] > largest_shoe_size
         largest_shoe_size = stats[:shoe]
